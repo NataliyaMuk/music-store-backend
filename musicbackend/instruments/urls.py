37a87be6d067e3f95485d6fcaf4cdfe7a6ddcde5
Sofya Osipova <avvada17@gmail.com>
2023-05-24 11:52:46 +0300

    последняя версия

--- musicbackend/instruments/urls.py
+++ musicbackend/instruments/urls.py
@@ -1,8 +1,8 @@
+"""urls"""
 from django.urls import include, path
 from rest_framework import routers
 
 from .views import BlogViewSet, ImagesViewSet, InstrumentsViewSet, SubCatViewSet
-
 router = routers.DefaultRouter()
 router.register(r"instruments", InstrumentsViewSet, basename="instruments")
 router.register(r"blog", BlogViewSet, basename="blog")
